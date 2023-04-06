db6d2b4316ef2b7648212974e2a6f0f12cdd185f
Elyes <126920308+ETechyDev@users.noreply.github.com>
2023-04-06 15:27:36 +0100

    Create Zrand.h

---
+++ Zrand.h
@@ -0,0 +1,60 @@
+#include<iostream>
+using namespace std;
+
+class zrand
+{
+    public: static char GetRandomChar(string text)
+    {
+        const int length = text.length();
+
+        char* split = new char[length + 1];
+
+        strcpy(split, text.c_str());
+
+        srand(time(0));
+        int u = rand() % length;
+
+        return split[u];
+    }
+
+    public: static string GetRandomString(string text[])
+    {        
+        int n = 1;
+        for(int i = 0; i < n; i++)
+        {
+            if(text[i] != "")
+            {
+                n += 1;
+            }
+        }
+        srand(sin(time(0)) / 0.01);
+        int y = rand() % (n-1);
+        
+        return text[y];
+    }
+
+    public: static int GetRandomLimitedInt(int min , int max)
+    {
+        srand(time(0));
+        int y = rand() % max;
+        while(y < min)
+        {
+            y = rand() % max;
+        }
+        return y;
+    }
+
+    public: static float GetRandomLimitedFloat(float min, float max)
+    {
+        srand(time(0));
+        float y = (pow((rand() % int(max)), 2)) / max;
+        while(y < min || y > max)
+        {
+            srand(time(0));
+            y = (pow((rand() % int(max)), 2)) / max;
+        }
+        return y;
+    }
+};
+
+static zrand ZRand;
